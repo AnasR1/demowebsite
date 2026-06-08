3348c174059a5757298ae470aee3dae5dbb6610c
anas <rahmanabduranas@gmail.com>
2026-06-08 13:41:15 -0400

    image updates

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -6,8 +6,8 @@ export default function listings() {
     <h1>Listings Page</h1>
     <p>Welcome to the listings page where we sell houses for real!</p>
     <br />
-    <ListingCard id={1} title="Cozy Apartment" description="A cozy apartment in the city center." price={1200} imageUrl=".././templogo.webp" />
+    <ListingCard id={1} title="Cozy Apartment" description="A cozy apartment in the city center." price={1200} imageUrl="/templogo.webp" />
     <br />
-    <ListingCard id={2} title="Spacious House" description="A spacious house with a garden." price={2500} imageUrl=".././templogo.webp" />
+    <ListingCard id={2} title="Spacious House" description="A spacious house with a garden." price={2500} imageUrl="/templogo.webp" />
   </div>;
 }
