9ce4ff6d4d6bf9fab27009fc10ebab596de93f1c
anas <rahmanabduranas@gmail.com>
2026-08-04 16:39:33 -0400

    Added proxy to prevent logged out user from rendering admin pages

---
+++ proxy.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function proxy(req: NextRequest) {
+  const sessionId = req.cookies.get('sessionId');
+
+  if (!sessionId) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+    headers: { cookie : `sessionId=${sessionId.value}` },
+  });
+
+  if (!response.ok) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+  
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
